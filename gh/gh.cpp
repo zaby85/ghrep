1fb4aa00848521bfe05d02ede2bcbe3441c614d5
ievgeshua <eu.lozhko@yandex.ru>
2025-06-23 10:48:24 +0300

    Добавьте файлы проекта.

---
+++ gh/gh.cpp
@@ -0,0 +1,11 @@
+﻿// gh.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Bye-bye"
+}
+
